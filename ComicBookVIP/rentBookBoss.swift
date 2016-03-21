e5ad70add6f01ad6c6713946b267cedf75877083
韓家豪 <white8017@hotmail.com>
2016-03-21 18:02:44 +0800

    萬惡的租書

---
+++ ComicBookVIP/rentBookBoss.swift
@@ -0,0 +1,26 @@
+//
+//  rentBookBoss.swift
+//  ComicBookVIP
+//
+//  Created by 韓家豪 on 2016/3/21.
+//  Copyright © 2016年 Mako. All rights reserved.
+//
+
+import UIKit
+
+class rentBookBoss: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
